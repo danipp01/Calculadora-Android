8d6207dec28d4433286d2a73dbc7b116de916706
Daniel <danielperezpedrosa@gmail.com>
2021-05-07 16:52:13 +0200

    Subida ejercicio resulto

--- Calculadora/app/src/main/java/com/example/calculadora/Principal.java
+++ Calculadora/app/src/main/java/com/example/calculadora/Principal.java
@@ -2,78 +2,105 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class Principal extends AppCompatActivity {
-    @Override
+    String oper = "+";
+    String ultimoNumero = "";
+    EditText edit;
+    Boolean operacionNueva = true;
+
+    @SuppressLint("WrongViewCast")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        edit = findViewById(R.id.textView2);
     }
-    public class Cuenta {
-        double numero1;
-        double numero2;
-        double resultado;
-        char operacion;
-        public Cuenta(double numero1, double numero2, char operacion) {
-            this.numero1 = numero1;
-            this.numero2 = numero2;
-            this.operacion = operacion;
+    public void numeros (View view){
+        if(operacionNueva)
+            edit.setText("");
+        operacionNueva = false;
+        String numero = edit.getText ().toString();
+        switch (view.getId()){
+            case R.id.N0:
+                numero += "0";
+                break;
+            case R.id.N1:
+                numero += "1";
+                break;
+            case R.id.N2:
+                numero += "2";
+                break;
+            case R.id.N3:
+                numero += "3";
+                break;
+            case R.id.N4:
+                numero += "4";
+                break;
+            case R.id.N5:
+                numero += "5";
+                break;
+            case R.id.N6:
+                numero += "6";
+                break;
+            case R.id.N7:
+                numero += "7";
+                break;
+            case R.id.N8:
+                numero += "8";
+                break;
+            case R.id.N9:
+                numero += "9";
+                break;
         }
 
-        public void Resultado(){
-            System.out.println(this.numero1+" "+this.operacion+" "+this.numero2+" = "+this.resultado);
+        edit.setText(numero);
+    }
 
+    public void Operaciones (View view){
+        operacionNueva = true;
+        ultimoNumero = edit.getText().toString();
+        switch (view.getId()){
+            case R.id.Division: oper = "/";
+                break;
+            case R.id.Resta: oper = "-";
+                break;
+            case R.id.Suma: oper = "+";
+                break;
+            case R.id.Multiplicacion: oper = "*";
+                break;
         }
+    }
 
-        public class Suma extends Cuenta{
-
-            double suma;
-
-            public Suma(double numero1, double numero2) {
-
-                super(numero1, numero2, '+');
-                this.suma = numero1 + numero2;
-                this.setResultado(this.suma);
-            }
+    public void Resultado (View view){
+        String nuevoNumero = edit.getText().toString();
+        double resultado = 0.0;
+        switch (oper){
+            case "+":
+                resultado = Double.parseDouble(ultimoNumero) + Double.parseDouble(nuevoNumero);
+                break;
+            case "-":
+                resultado = Double.parseDouble(ultimoNumero) - Double.parseDouble(nuevoNumero);
+                break;
+            case "*":
+                resultado = Double.parseDouble(ultimoNumero) * Double.parseDouble(nuevoNumero);
+                break;
+            case "/":
+                resultado = Double.parseDouble(ultimoNumero) / Double.parseDouble(nuevoNumero);
+                break;
         }
+        edit.setText(resultado + "");
+    }
 
-        public class Resta extends Cuenta{
+    public void Borrar (View view){
+        edit.setText("0");
+        operacionNueva = true;
 
-            double resta;
-
-            public Resta(double numero1, double numero2) {
-
-                super(numero1, numero2, '-');
-                this.resta = numero1 - numero2;
-                this.setResultado(this.resta);
-            }
-        }
-
-        public class Multiplicacion extends Cuenta{
-
-            double multiplicacion;
-
-            public Multiplicacion(double numero1, double numero2) {
-
-                super(numero1, numero2, '*');
-                this.multiplicacion = numero1 * numero2;
-                this.setResultado(this.multiplicacion);
-            }
-        }
-
-        public class Division extends Cuenta{
-
-            double division = 0;
-
-            public Division(double numero1, double numero2) {
-
-                super(numero1, numero2, '/');
-                if(numero2==0) System.out.println("¡ERROR! No se puede dividir entre cero");
-                else this.div = numero1 / numero2;
-                this.setResultado(this.division);
-            }
-        }
-}}
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Calculadora-Android
+
+Esta calculadora será usada para realizar las operaciones ordinarias: suma, resta, multiplicación y división.
+La estructura de la aplicación es la siguiente: 
+
+El uso normal de esta calculadora será el siguiente:
+1. Pulsa los botón del primer número que quiera introducir.
+2. Pulse el botón de la operación a realizar.
+3. Pulse los botones para añadir el segundo número.
+4. Pulse el botón de = para que aparezca el resultado.
+5. Si quiere seguir haciendo operaciones con el resultado introduzca otro número y si quiere borrar pulse el botón de C (Borrar). 
